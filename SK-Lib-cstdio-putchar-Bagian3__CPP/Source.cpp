a78804f46152602ff5516ba6c38fa0d030735e22
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-07-27 20:11:12 +0700

    Initial commit

---
+++ SK-Lib-cstdio-putchar-Bagian3__CPP/Source.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <conio.h>
+
+/*
+    Source by Programiz (https://www.programiz.com)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int main() {
+    for (int i = 48; i < 58; i++) {
+        putchar(i);
+        putchar(' ');
+    }
+
+    _getch();
+    return 0;
+}
